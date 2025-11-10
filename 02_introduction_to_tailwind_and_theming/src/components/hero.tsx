73caca6c74787d6b19ca108c19c3a93b00a708b5
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-10 20:09:33 +0530

    Add Theming

--- 02_introduction_to_tailwind_and_theming/src/components/hero.tsx
+++ 02_introduction_to_tailwind_and_theming/src/components/hero.tsx
@@ -3,22 +3,22 @@ import React from "react"
 export const Hero = () => {
   return (
     <div className="my-40 flex w-full flex-col items-center justify-center">
-      <h1 className="max-w-2xl bg-linear-to-b from-neutral-50 to-neutral-500 bg-clip-text text-center text-7xl leading-tight font-bold tracking-tight text-transparent">
+      <h1 className="max-w-2xl bg-linear-to-b from-neutral-50 to-neutral-500 bg-clip-text text-center text-7xl leading-tight font-bold tracking-tight text-transparent font-victorMono">
         Unleash the power of intuitive finance
       </h1>
-      <p className="mx-auto mt-10 max-w-3xl text-center text-xl text-neutral-500 selection:bg-white">
-        Say goodbye to the outdated financial tools. Every small business owner,
-        regardless of the background, can now manage their business like a pro.
+      <p className="mx-auto mt-10 max-w-3xl text-center text-xl text-neutral-500 selection:bg-white font-momoSignature">
+        Say goodbye to the <span className="text-primary">outdated</span> financial tools. Every small business owner,
+        regardless of the background, can now manage their <span className="text-primary">business</span> like a pro.
         Simple. Intuitive. And never boring.
       </p>
       <div className="mt-12 flex w-full max-w-lg justify-center">
         <input
           type="text"
-          className="mr-4 flex-1 rounded-xl border border-neutral-600 px-4 text-white placeholder:text-neutral-500 focus:outline-none focus:ring-1 focus:ring-sky-500 transition duration-200"
+          className="mr-4 flex-1 rounded-xl border border-neutral-600 px-4 text-white placeholder:text-neutral-500 focus:outline-none focus:ring-1 focus:ring-primary transition duration-200"
           placeholder="Enter your email"
         />
         <button className="relative cursor-pointer rounded-xl border border-neutral-700 px-4 py-2 text-white">
-          <div className="absolute inset-x-0 -bottom-px h-px w-full bg-linear-to-r from-transparent via-sky-600 to-transparent"></div>
+          <div className="absolute inset-x-0 -bottom-px h-px w-full bg-linear-to-r from-transparent via-primary to-transparent"></div>
           Join the waitlist
         </button>
       </div>
