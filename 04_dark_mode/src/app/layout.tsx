2b510caf407fa996600e8af6cf6cc097c49c1b89
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-15 18:40:12 +0530

    Created a Card

--- 04_dark_mode/src/app/layout.tsx
+++ 04_dark_mode/src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased [--pattern-fg:var(--color-neutral-950)]/5`}
       >
-        {children}
+        <main className="bg-background text-foreground h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
