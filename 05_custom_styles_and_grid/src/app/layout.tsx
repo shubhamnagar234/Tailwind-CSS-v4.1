073dbd8dae4b29f3c4742a838e0e88ecabdc19c9
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-18 20:33:47 +0530

    Added Sun and Moon Icon

--- 05_custom_styles_and_grid/src/app/layout.tsx
+++ 05_custom_styles_and_grid/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main className="bg-background text-foreground h-screen">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
