dcdded278e1c91d2c447af22feca849f8e826040
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-21 22:56:47 +0530

    Created Component

---
+++ 06_css_properties/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Component from "@/components/component";
+
+export default function Home() {
+  return (
+   <main className="h-screen max-w-4xl mx-auto flex items-center"><Component /></main>
+  );
+}
