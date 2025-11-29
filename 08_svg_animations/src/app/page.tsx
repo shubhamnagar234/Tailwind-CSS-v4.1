13f2e5f02616c281c838d3f9197a829b449c50f9
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-29 20:39:04 +0530

    Motion Animate bell

--- 08_svg_animations/src/app/page.tsx
+++ 08_svg_animations/src/app/page.tsx
@@ -2,8 +2,9 @@ import { SVGComponent } from "@/components/svg-component";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen max-w-4xl bg-gray-50 mx-auto flex items-center flex-col py-40">
       <SVGComponent />
     </div>
   );
 }
+
