2f4b80e47e637168de7d327fd0ce86d721e636e8
Shubham Nagar <shubhamnagar234@gmail.com>
2025-11-30 19:50:08 +0530

    Added SVGLines Component

---
+++ 08_svg_animations/src/components/svg-lines.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { motion } from "motion/react";
+
+export const SVGLines = () => {
+  return (
+    <motion.div
+      whileHover="animate"
+      className="h-40 w-40 bg-white flex rounded-md shadow-md items-center justify-center"
+    >
+     
+    </motion.div>
+  );
+};
