3948b1bb04980adcfef3b5cb03043c3aaf3ee3ad
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-04 17:44:58 +0530

    Created Card

--- 09_functions_and_directive/src/app/page.tsx
+++ 09_functions_and_directive/src/app/page.tsx
@@ -1,65 +1,74 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="h-screen w-full max-w-4xl mx-auto py-20 bg-neutral-50">
+      <Header />
+      <div className="grid grid-cols-4 gap-4 my-10">
+        <Column>
+          <Card src="/images/1.jpg" alt="1" className="" />
+          <Card src="/images/5.jpg" alt="2" className="" />
+          <Card src="/images/3.jpg" alt="3" className="" />
+        </Column>
+        <Column>
+          <Card src="/images/4.jpg" alt="4" className="" />
+          <Card src="/images/5.jpg" alt="5" className="" />
+          <Card src="/images/6.jpg" alt="6" className="" />
+        </Column>
+        <Column>
+          <Card src="/images/1.jpg" alt="7" className="" />
+          <Card src="/images/8.jpg" alt="8" className="" />
+          <Card src="/images/3.jpg" alt="9" className="" />
+        </Column>
+        <Column>
+          <Card src="/images/4.jpg" alt="4" className="" />
+          <Card src="/images/5.jpg" alt="5" className="" />
+          <Card src="/images/6.jpg" alt="6" className="" />
+        </Column>
+      </div>
     </div>
   );
 }
+
+const Card = ({
+  src,
+  alt,
+  className,
+}: {
+  src: string;
+  alt: string;
+  className: string;
+}) => {
+  return (
+    <div className="card">
+      <Image
+        src={src}
+        alt={alt}
+        height={500}
+        width={500}
+        className={cn("", className)}
+      />
+    </div>
+  );
+};
+
+const Column = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tighter text-neutral-700">
+        Bento grids are cool, you should try it sometimes.
+      </h1>
+      <p className="text-base text-neutral-500 max-w-4xl mt-4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi ex
+        aliquid aperiam provident, laboriosam hic corrupti. Labore, libero
+        recusandae unde nihil similique ipsum ad qui nesciunt a debitis culpa
+        ipsa.
+      </p>
+    </>
+  );
+};
