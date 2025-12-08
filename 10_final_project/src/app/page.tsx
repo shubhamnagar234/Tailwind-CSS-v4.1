cb71773a4cf24511757b842f4a04c079a63560ee
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-08 23:06:12 +0530

    Added Heading, SubHeading and Hero components

--- 10_final_project/src/app/page.tsx
+++ 10_final_project/src/app/page.tsx
@@ -1,9 +1,11 @@
 // import { ModeToggle } from "@/components/mode-toggle";
 
+import { Hero } from "@/components/hero";
+
 export default function Home() {
   return (
    <div className="min-h-screen font-display">
-   
+   <Hero />
     {/* <ModeToggle />
     <p className="font-inter">Agents that do the work. Approvals that keep you safe.</p> */}
    </div>
