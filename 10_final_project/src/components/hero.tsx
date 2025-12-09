499d58c7129e96852b5e4c5a48aa006611d0d5ac
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-09 18:28:04 +0530

    Added Buttons

--- 10_final_project/src/components/hero.tsx
+++ 10_final_project/src/components/hero.tsx
@@ -1,6 +1,8 @@
+import { Button } from "./button";
 import { Container } from "./container";
 import { Heading } from "./heading";
 import { SubHeading } from "./subheading";
+import Link from "next/link";
 
 export const Hero = () => (
   <div className="pt-10 md:pt-20 lf:pt-32">
@@ -12,6 +14,12 @@ export const Hero = () => (
         Deploy AI Agents that plan, act through your tools, and remote
         outcomes-without changing how your teams work.
       </SubHeading>
+      <div className="flex items-center gap-6">
+        <Button>Start your free trial</Button>
+        <Button variants="secondary">
+          <Link href="#">View role based demos</Link>
+        </Button>
+      </div>
     </Container>
   </div>
 );
